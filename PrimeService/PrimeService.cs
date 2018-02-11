a5c97d808fdeee2eb9bd8914a5cf29c7598d67a1
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-11 14:29:30 -0600

    final refactor

--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -6,10 +6,21 @@ namespace Prime.Services
     {
         public bool IsPrime(int candidate)
         {
-            throw new NotImplementedException(
-                "Please create a test first.`"
-            );
+            if (candidate < 2) return false;
+
+            if (candidate == 2) return true;
+
+            if (candidate % 2 == 0) return false;
+
+            var boundary = Math.Floor(Math.Sqrt(candidate));
+
+            for (var i = 3; i < boundary; i++)
+            {
+                if (candidate % i == 0) return false;
+            }
+
+            return true;
+
         }
     }
 }
-`
