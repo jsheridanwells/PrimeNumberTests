272d3fd830dd4b8df3aad3bb2a4f13c2892e552a
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-11 14:18:42 -0600

    returns true for prime numbers, al tests pass

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -18,14 +18,24 @@ namespace Prime.UnitTests.Services
         var result = _primeService.IsPrime(1);
         Assert.IsFalse(result, "1 should not be prime.");
       }
-      
+
       [TestCase(-1)]
       [TestCase(0)]
       [TestCase(1)]
       public void ReturnFalseGivenValuesLessThan2(int value)
-        {
-            var result = _primeService.IsPrime(value);
-            Assert.IsFalse(result, $"{value} should not be prime");
-        }
+      {
+          var result = _primeService.IsPrime(value);
+          Assert.IsFalse(result, $"{value} should not be prime");
+      }
+      
+      [TestCase(2)]
+      [TestCase(3)]
+      [TestCase(5)]
+      public void ReturnTrueGivenPrimeNumbers(int value)
+      {
+        var result = _primeService.IsPrime(value);
+        Assert.IsTrue(result, $"{value} should be prime");
+      }
+
     }
 }
